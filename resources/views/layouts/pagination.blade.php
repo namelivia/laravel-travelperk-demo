a2a16c52bb0edd473e834376328fe00800a8d805
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-09-05 17:30:24 +0200

    Limit pagination size

--- resources/views/layouts/pagination.blade.php
+++ resources/views/layouts/pagination.blade.php
@@ -4,9 +4,12 @@
         <a class="page-link" href="?page={{($response->offset / $response->limit) - 1}}">Previous</a>
     </li>
     @foreach (range(0, ($response->total/$response->limit)) as $page)
-        <li class="page-item {{ $loop->index == ($response->offset / $response->limit) ? 'active' : ''}}">
-            <a class="page-link" href="?page={{$page}}">{{$page}}</a>
-        </li>
+        @if ($loop->index > ($response->offset / $response->limit) - 10 &&
+            $loop->index < ($response->offset / $response->limit) + 10)
+            <li class="page-item {{ $loop->index == ($response->offset / $response->limit) ? 'active' : ''}}">
+                <a class="page-link" href="?page={{$page}}">{{$page}}</a>
+            </li>
+        @endif
     @endforeach
     <li class="page-item {{ $response->offset + $response->limit > $response->total ? 'disabled' : ''}}">
         <a class="page-link" href="?page={{$response->offset + 1}}">
