618a1e45b99a066bb1b9029e810051587f11f3b8
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-09-17 08:24:09 +0200

    Fix pagination

--- resources/views/layouts/pagination.blade.php
+++ resources/views/layouts/pagination.blade.php
@@ -3,7 +3,7 @@
     <li class="page-item {{ $response->offset == 0 ? 'disabled' : ''}}">
         <a class="page-link" href="?page={{($response->offset / $response->limit) - 1}}">Previous</a>
     </li>
-    @foreach (range(0, ($response->total/$response->limit)) as $page)
+    @foreach (range(0, ($response->total > $response->limit ? $response->total/$response->limit : 0)) as $page)
         @if ($loop->index > ($response->offset / $response->limit) - 10 &&
             $loop->index < ($response->offset / $response->limit) + 10)
             <li class="page-item {{ $loop->index == ($response->offset / $response->limit) ? 'active' : ''}}">
